d6109e3965325d5f2b4453135aa25621dd94af91
Marcelo de Abreu <109244891+marcelodeabreu@users.noreply.github.com>
2023-06-27 18:55:37 -0300

    Add files via upload
    Form example for iOS.

---
+++ Form Example/Form_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Form_ExampleApp.swift
+//  Form Example
+//
+//  Created by Marcelo de Abreu on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Form_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
